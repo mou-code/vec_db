0cc3d5d73fd25fb28b1b9e9af9133d2841818f5f
Mennatallah Ahmed <mennaahmed0701@gmail.com>
2024-12-12 12:15:27 +0200

    Update vec_db.py

--- vec_db.py
+++ vec_db.py
@@ -119,7 +119,7 @@ class VecDB:
         elif num_records == 15*10**6:
           n_probe=20
         elif num_records==20*10**6:
-            n_probe=55
+            n_probe=20
         # print("n_probe=",n_probe)
         # 1. Getting nearest centroids in first level
         #get index data
@@ -140,7 +140,7 @@ class VecDB:
         elif num_records == 15*10**6:
           n_probes_l2=310
         elif num_records==20*10**6:
-            n_probes_l2=700
+            n_probes_l2=30
         for _,centroid_idx in nearest_centroids:
             # print("cluster num#",centroid_idx)
             # now within a cluster, let's open file of the cluster
