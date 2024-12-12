3fa33bd4e0931a4787fd14ad644ea5315d94eb65
Mennatallah Ahmed <mennaahmed0701@gmail.com>
2024-12-13 00:00:31 +0200

    Update vec_db.py

--- vec_db.py
+++ vec_db.py
@@ -113,7 +113,7 @@ class VecDB:
         index_path_level1=f"{saved_db_name}/level1_centroids_{self.index_path}"
 
         if num_records == 10**6:
-           n_probe =80
+           n_probe =100
         elif num_records ==10**7:
           n_probe=40
         elif num_records == 15*10**6:
