3bd9d00f80a4f18dd7d2028aa03c9333a30832b2
Mennatallah Ahmed <mennaahmed0701@gmail.com>
2024-12-12 14:15:27 +0200

    Update vec_db.py

--- vec_db.py
+++ vec_db.py
@@ -134,7 +134,7 @@ class VecDB:
         # 2. Getting nearest centroids in second level
         top_k_heap=[]
         if num_records == 10**6:
-           n_probes_l2 =50
+           n_probes_l2 =30
         elif num_records ==10**7:
           n_probes_l2=120
         elif num_records == 15*10**6:
