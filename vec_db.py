b51becc92ce3e453d475700951c263a004ec374a
Mennatallah Ahmed <mennaahmed0701@gmail.com>
2024-12-13 22:58:28 +0200

    Update vec_db.py

--- vec_db.py
+++ vec_db.py
@@ -121,7 +121,7 @@ class VecDB:
         if num_records == 10**6:
            n_probe =50
         elif num_records ==10**7:
-          n_probe=85
+          n_probe=80
         elif num_records == 15*10**6:
           n_probe=78
         elif num_records==20*10**6:
